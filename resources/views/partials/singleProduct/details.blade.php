3bc3ec50dc9f3997e4ed7ddfb4f0fe51c8ca1b51
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-15 11:25:10 -0400

    Update details.blade.php

--- resources/views/partials/singleProduct/details.blade.php
+++ resources/views/partials/singleProduct/details.blade.php
@@ -111,7 +111,7 @@
                            {{--  {{dd("/storage/".json_decode($product->instructions)[0]->download_link)}} --}}
                            
                         <p> Looking for product installation instructions? 
-                            @if(empty($product->instructions) ) 
+                            @if(empty($product->instructions) || $product->instructions == null ) 
                             {{-- {{dd(productInstructions($product->instructions))}} --}}
                               <span>
                                     {{--  --}}<a >Instuctions will be uploaded soon.</a>
