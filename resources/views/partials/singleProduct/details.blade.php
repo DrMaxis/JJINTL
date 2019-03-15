20df3452f0f92458c5d3d4bb813fa8abd76e55e0
DrMaxis <Grandmasterantwii@gmail.com>
2019-03-15 10:56:42 -0400

    Update details.blade.php

--- resources/views/partials/singleProduct/details.blade.php
+++ resources/views/partials/singleProduct/details.blade.php
@@ -113,7 +113,7 @@
                         <p> Looking for product installation instructions? 
                             @if(!empty($product->instructions) ) 
                             <span>
-                                <a href="/storage/{{productInstructions($product->instructions)[0]['download_link']}}"> Click here to download</a>
+                                <a href="/storage/{{productInstructions($product->instructions)[0]->download_link}}"> Click here to download</a>
                             </span>  
                             @else  
                             <span>
