47e021f16d7add2c175aa9024951912a49659aed
DrMaxis <Grandmasterantwii@gmail.com>
2019-04-05 08:57:19 -0400

    update

--- resources/views/partials/homepage/sliderArea.blade.php
+++ resources/views/partials/homepage/sliderArea.blade.php
@@ -17,7 +17,7 @@
                             <div class="carousel-inner" role="listbox">
                                 @foreach($products -> take (1) as $mainProduct) 
                               <div class="item active">
-                                    <a href="{{route('single-product', $mainProduct->slug)}}"><img id="simage"  class="h-100" src="{{productImage($mainProduct->image)}}" width="100%"></a>
+                                    <a href="{{route('single-product', $mainProduct->slug)}}"><img id="simage" style="margin:0 auto;" class="h-100" src="{{productImage($mainProduct->image)}}" width="100%"></a>
                                 <div style="position:initial; color:black;" class="carousel-caption">
                                   <h3>{{$mainProduct->name}}</h3>
                                   <p>{{$mainProduct->details}}<br></p>
@@ -29,7 +29,7 @@
                             $mainProduct->slug)
                             @else 
                             <div class="item">
-                                    <a href="{{route('single-product', $product->slug)}}"> <img id="simage" class="h-100" src="{{productImage($product->image)}}" width="100%"></a>
+                                    <a href="{{route('single-product', $product->slug)}}"> <img id="simage" style="margin:0 auto;" class="h-100" src="{{productImage($product->image)}}" width="100%"></a>
                                     <div style="position:initial; color:black;" class="carousel-caption">
                                       <h3>{{$product->name}}</h3>
                                       <p>{{$product->details}}<br></p>
@@ -40,11 +40,11 @@
                             </div>
                             <!-- Controls -->
                             <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                              <span class="glyphicon glyphicon-chevron-left" style="color:black"  aria-hidden="true"></span>
                               <span class="sr-only">Previous</span>
                             </a>
                             <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                              <span class="glyphicon glyphicon-chevron-right" style="color:black" aria-hidden="true"></span>
                               <span class="sr-only">Next</span>
                             </a>
                           </div>
