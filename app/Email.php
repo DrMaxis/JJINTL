8fc48a34be000ebaa6f87829970879e39b13bf33
DrMaxis <Grandmasterantwii@gmail.com>
2019-04-19 08:05:56 -0400

    Update Email.php

--- app/Email.php
+++ app/Email.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Email extends Model
 {
  protected $table = "emails";   
-protected $fillable = ['name','email','company' ,'subject' ,'message'];
+protected $fillable = ['name','email','phone','company' ,'subject' ,'message'];
    
 }
